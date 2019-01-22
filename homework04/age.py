4f28fe79f4925defc4b988074317779e18c1ec40
raspdk <46626680+raspdk@users.noreply.github.com>
2019-01-23 00:40:32 +0300

    'Исправления'

--- homework04/age.py
+++ homework04/age.py
@@ -20,18 +20,18 @@ def age_predict(user_id: int) -> Optional[float]:
     fr_list = get_friends(user_id, 'bdate')
     age_list = []
     for fr in fr_list:
-		user = User(**fr)
-		if user.bdate:
-			try:
-				bd = dt.datetime.strptime(user.bdate, '%d.%m.%Y')
-			except:
-				continue
-			age = cur_date.year - bd.year
-			if cur_date.month < bd.month:
-				age -= 1
-			elif (cur_date.month == bd.month) and (cur_date.day <= bd.day):
-				age -= 1
-			age_list.append(age)
+    	user = User(**fr)
+    	if user.bdate:
+    		try:
+    			bd = dt.datetime.strptime(user.bdate, '%d.%m.%Y')
+    		except:
+    			continue
+    		age = cur_date.year - bd.year
+    		if cur_date.month < bd.month:
+    			age -= 1
+    		elif (cur_date.month == bd.month) and (cur_date.day <= bd.day):
+    			age -= 1
+    		age_list.append(age)
 
     if age_list:
     	return float(median(age_list))
