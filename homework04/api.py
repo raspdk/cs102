d4bd9151b3b3a33fd37499def09484488602b47a
raspdk <46626680+raspdk@users.noreply.github.com>
2019-01-22 18:36:49 +0300

    Реализована функция get_friends()

--- homework04/api.py
+++ homework04/api.py
@@ -3,6 +3,9 @@ import time
 
 import config
 
+domain = "https://api.vk.com/method"
+access_token = # PUT YOUR ACCESS TOKEN HERE
+user_id = # PUT USER ID HERE
 
 def get(url, params={}, timeout=5, max_retries=5, backoff_factor=0.3):
     """ Выполнить GET-запрос
@@ -25,8 +28,17 @@ def get_friends(user_id, fields):
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert isinstance(fields, str), "fields must be string"
     assert user_id > 0, "user_id must be positive integer"
-    # PUT YOUR CODE HERE
+    
+    query_params = {
+        'domain' : domain,
+        'access_token': access_token,
+        'user_id': user_id,
+        'fields': fields
+    }
 
+    query = "{domain}/friends.get?access_token={access_token}&user_id={user_id}&fields={fields}&v=5.53".format(**query_params)
+    response = requests.get(query, query_params)
+    return response.json()
 
 def messages_get_history(user_id, offset=0, count=20):
     """ Получить историю переписки с указанным пользователем
